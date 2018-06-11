6fda6d58245470cb501796b412e574a84e4d9692
NoobZombieTR <38327391+NoobZombieTR@users.noreply.github.com>
2018-06-11 15:40:42 +0300

    Update information.js

--- komutlar/information.js
+++ komutlar/information.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
